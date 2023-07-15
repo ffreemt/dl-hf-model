b39d2df74043105c2a7c2bd1242a7cb58cca84c1
ffreemt <yucongo+fmt@gmail.com>
2023-07-15 21:06:03 +0800

    Bump version from 0.1.0a0 to 0.1.0a1

--- dl_hf_model/__init__.py
+++ dl_hf_model/__init__.py
@@ -1,5 +1,5 @@
 """Init."""
-__version__ = "0.1.0a0"
+__version__ = "0.1.0a1"
 from .dl_hf_model import dl_hf_model
 
 __all__ = ("dl_hf_model",)
--- dl_hf_model/dl_hf_model.py
+++ dl_hf_model/dl_hf_model.py
@@ -1,12 +1,14 @@
 """Define dl_hf_model."""
 from pathlib import Path
+from typing import Tuple
 from urllib.parse import urlparse
 
 from huggingface_hub import hf_hub_download
 from loguru import logger
 
 
-def get_repo_id_model_file_revision(url: str) -> (str, str, str):
+def get_repo_id_model_file_revision(url: str) -> Tuple[str, str, str]:
+    """Fetch repo_id, mode_file, revision."""
     model_file = Path(url).name
     _ = urlparse(url).path.strip("/").split("/")
     repo_id = "/".join(_[:2])
@@ -19,14 +21,14 @@ def get_repo_id_model_file_revision(url: str) -> (str, str, str):
 def dl_hf_model(
     url: str,
     local_dir: str = "models",
-):
+) -> Tuple[str, float]:
     """Download and cache a hf model gievn by url, save to local_dir."""
     repo_id, model_file, revision = get_repo_id_model_file_revision(url)
     try:
         model_file = hf_hub_download(
             repo_id=repo_id,
             filename=model_file,
-            local_dir="models",
+            local_dir=local_dir,  # "models"
             revision=None if revision in ["main"] else revision
             # local_dir_use_symlinks=True,
         )
@@ -34,4 +36,10 @@ def dl_hf_model(
         logger.error(exc)
         raise
 
-    return model_file
+    try:
+        st_size = round(Path(model_file).stat().st_size / 10**9, 2)
+    except Exception as exc:
+        logger.error(exc)
+        raise
+
+    return model_file, st_size
